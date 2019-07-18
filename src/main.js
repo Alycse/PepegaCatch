780b3356a8c48c85a79b1aed97cb5e411cae1063
Alycse <alycse@gmail.com>
2019-07-18 17:31:18 +0800

    Removed background.js and moved its code into main.js

--- src/main.js
+++ src/main.js
@@ -15,6 +15,44 @@ var pepegaFullArmySound = new Audio(chrome.runtime.getURL("sounds/pepega-full-ar
 var pepegaLevelSound = new Audio(chrome.runtime.getURL("sounds/pepega-level.ogg"));
 var pepegaFusionSound = new Audio(chrome.runtime.getURL("sounds/pepega-fusion.ogg"));
 
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if(request.message == "get-wild-pepega"){
+			sendResponse({ isSiteFiltered: isSiteFiltered(request.location), wildPepega: getWildPepega(request.location) });
+		}else if(request.message == "catch-wild-pepega"){
+			catchWildPepega(request.wildPepegaTypeId, request.location);
+			sendResponse();
+		}else if(request.message == "update-all-popup-displays"){
+			updateAllPopupDisplays();
+			sendResponse();
+		}else if(request.message == "update-player-encounter-rate"){
+			updatePlayerEncounterRate();
+			sendResponse();
+		}else if(request.message == "release-player-pepega"){
+			releasePlayerPepega(request.playerPepegaId);
+			sendResponse();
+		}else if(request.message == "update-settings"){
+			updateSettings(request.filteredSitesText, request.enableSounds, 
+				request.enablePepegaCatchReleaseNotifications, request.enableRankUpNotifications, request.recordOrigin);
+			sendResponse();
+		}else if(request.message == "update-player-army-name"){
+			updatePlayerArmyName(request.playerArmyName);
+			sendResponse();
+		}else if(request.message == "buy-pepega-slot"){
+			buyPepegaSlot();
+			sendResponse();
+		}
+	}
+);
+
+chrome.tabs.onActivated.addListener(function(tab) {
+	chrome.tabs.getSelected(function(tab) {
+		var location = new Object();
+		location.hostname = tab.url;
+		isSiteFiltered(location);
+	});
+});
+
 var popup = {
 	get isOpened (){
 		return chrome.extension.getViews({ type: "popup" }).length > 0;
