2362d840b4434434a596bf95e234d99efef04263
Alycse <alycse@gmail.com>
2019-07-23 22:53:27 +0800

    Added Battle Breakdown from catching Wild Pepegas

---
+++ src/popup/battleBreakdown/battleBreakdown.js
@@ -0,0 +1,71 @@
+var browser = chrome;
+var browserRuntime = browser.runtime;
+var browserStorage = browser.storage.local;
+
+browserStorage.get(["recentBattleBreakdown"], function(results) {
+    console.log("is nulL?: " + results.recentBattleBreakdown);
+    
+    var battleBreakdown = results.recentBattleBreakdown;
+    if(battleBreakdown){
+        var battleBreakdownTitleText = "";
+        var battleBreakdownText = "";
+
+        var player = battleBreakdown.player;
+        var wildPepega = battleBreakdown.wildPepega;
+        var rounds = battleBreakdown.rounds;
+
+        battleBreakdownTitleText += "<p>" + player.armyName + " <span id=\"versus\">VS.</span> Wild " + wildPepega.name + "</p>";
+        battleBreakdownTitleText += "<p><span id=\"wildPepegaTitlePower\">Wild " + wildPepega.name + "'s Power: </span>" + wildPepega.totalPower + "</p>";
+
+        for(var i = 0; i < rounds.length; i++){
+
+            var round = rounds[i];
+
+            battleBreakdownText += 
+                "<p><b><span class=\"ally\">" + round.playerPepega.name + "</span></b> used <span class=\"attack\">" + 
+                round.playerPepega.attack + "</span>!, dealing <b>" + round.playerPepega.power + " damage</b> to <b><span class=\"enemy\">Wild " + 
+                wildPepega.name + "</span></b>!</p>";
+
+            if(!round.roundPlayerWon){
+                battleBreakdownText += "<p><b><span class=\"enemy\">Wild " + wildPepega.name + "</span></span></b> has <b>" + 
+                round.wildPepega.remainingPower + " remaining power</b>.</p>";
+
+                battleBreakdownText += "<p><b><span class=\"enemy\">Wild " + wildPepega.name + "</span></b> used <span class=\"attack\">" + round.wildPepega.attack + 
+                    "</span>!, dealing <b>" + wildPepega.totalPower + " damage</b> to <b><span class=\"ally\">" + round.playerPepega.name + "</span></b>!</p>";
+
+                battleBreakdownText += "<p><b><span class=\"ally\">" + round.playerPepega.name + "</span></b> fainted!</p>";
+            }
+
+        }
+        
+        if(rounds.length == 0 || !rounds[rounds.length - 1].roundPlayerWon){
+            battleBreakdownText += "<p><b><span class=\"ally\">" + player.rankTitle + "</span></b> attacked <b><span class=\"enemy\">Wild " + wildPepega.name + 
+                "</span></b>, dealing <b>" + player.rolledRankPower + " damage</b>!</p>";
+        }
+
+        if(wildPepega.remainingPower <= 0){
+            battleBreakdownText += "<p><b><span class=\"enemy\">Wild " + wildPepega.name + "</span></b> fainted!</p>";
+            battleBreakdownText += "<p><b><span class=\"ally\">" + player.rankTitle + "</span></b> successfully caught <b><span class=\"enemy\">Wild " +
+                wildPepega.name + "</span></b>!</p>";
+        }else{
+            battleBreakdownText += "<p><b><span class=\"enemy\">Wild " + wildPepega.name + "</span></b> has <b>" + wildPepega.remainingPower + " remaining power</b>.</p>";
+            battleBreakdownText += "<p><span id=\"lost\">You were unable to defeat "+wildPepega.name+"</span></p>";
+            if(rounds.length > 0){
+                battleBreakdownText += "<p><span id=\"lost\">All your Pepegas have fainted.</span></p>";
+            }
+            battleBreakdownText += "<p><span id=\"lost\">VI LOST!</span></p>";
+        }
+
+        document.getElementById("battleBreakdownTitle").innerHTML = battleBreakdownTitleText;
+        document.getElementById("battleBreakdown").innerHTML = battleBreakdownText;
+    }else{
+        document.getElementById("battleBreakdownTitle").innerHTML = "You haven't fought any Wild Pepegas yet!";
+        document.getElementById("battleBreakdown").innerHTML = "...";
+    }
+});
+
+function returnToHomeScreen(){
+	window.location.href=browserRuntime.getURL("src/popup/popup.html");
+}
+
+document.getElementById("returnToHome").addEventListener("click", returnToHomeScreen);
